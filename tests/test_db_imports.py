77dc16d52878796b8fbcd1b0595a65443fc5efa0
endiesworld <endiesworld@gmail.com>
2026-02-26 00:23:54 -0500

    fix: Added smoke test for db base and model

---
+++ tests/test_db_imports.py
@@ -0,0 +1,7 @@
+import importlib
+
+def test_db_base_module_imports():
+    importlib.import_module("buma.db.base")
+
+def test_db_models_module_imports():
+    importlib.import_module("buma.db.models")
